b8a778a7a5e6c82eb286614b74852a0b988b0767
Sheetal Gupta <sheetalgupta00001@gmail.com>
2020-07-23 22:31:30 +0530

    fix: prev arrow

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ $(document).ready( function () {
 		autoplaySpeed: 1500,
 		infinit: true,
 		arrows: true,
-		prevArrow:"<img class='a-right control-c next slick-prev' src='https://image.flaticon.com/icons/svg/892/892512.svg' style='width: 50px; height: 50px;'>",
+		prevArrow:"<img class='a-right control-c next slick-prev' src='https://image.flaticon.com/icons/svg/892/892512.svg' style='width: 50px; height: 50px; position: absolute; z-index: 1; '>",
 		nextArrow:"<img class='a-left control-c prev slick-next' src='https://image.flaticon.com/icons/svg/892/892528.svg' style='width: 50px; height: 50px;'>",
 		responsive: [
 		{
